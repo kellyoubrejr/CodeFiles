2d30e2eb0fc63a80cd5cf293299c37402e3428a1
WeiYang <weiy6141@gmail.com>
2026-07-01 17:34:29 +0800

    单据关闭关闭提示信息

---
+++ ERP/Zitn_CloseBill_Plugin/Zitn_CloseBill_Plugin/CloseBillNoWarnBillPlugIn.cs
@@ -0,0 +1,30 @@
+﻿using Kingdee.BOS.Core.Bill.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Util;
+using System.ComponentModel;
+
+namespace Zitn_CloseBill_Plugin
+{ 
+    /// <summary>
+    /// 【单据插件】单据界面关闭时不提示是否保存
+    /// </summary>
+    [Description("【单据插件】单据界面关闭时不提示是否保存"), HotUpdate]
+    public class CloseBillNoWarnBillPlugIn : AbstractBillPlugIn
+    {
+        public override void BeforeClosed(BeforeClosedEventArgs e)
+        {
+
+            base.BeforeClosed(e);
+
+            this.Model.DataChanged = false;
+
+            /*if (this.View.ParentFormView != null)
+            {
+                this.View.ParentFormView.Refresh();
+                this.View.SendAynDynamicFormAction(this.View.ParentFormView);
+            }*/
+
+
+        }
+    }
+}
