69ca3bc3831b1ddb0cc7b6254440343bdc9a051c
WeiYang <weiy6141@gmail.com>
2026-07-01 17:59:50 +0800

    采购提交检测供应商次数判断

---
+++ ERP/Zitn_PoSupplierJudgeCount_PluginNew/Zitn_PoSupplierJudgeCount_PluginNew/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("Zitn_PoSupplierJudgeCount_PluginNew")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Zitn_PoSupplierJudgeCount_PluginNew")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("b1a48bc4-b03f-4d75-bbb8-0629fc866867")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
